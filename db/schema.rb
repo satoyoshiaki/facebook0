adfe9eba770873bd4ff7b098ef8872fb38bc6f4c
佐藤良昭 <yoshiprograming@gmail.com>
2020-03-31 11:17:49 +0900

    ログイン機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_30_014410) do
+ActiveRecord::Schema.define(version: 2020_03_31_020727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "taitle"
+    t.text "context"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
