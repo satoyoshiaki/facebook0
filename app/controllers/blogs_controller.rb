37bb4321ce6f4f1726506fc5cda090dd480ff9c8
佐藤良昭 <yoshiprograming@gmail.com>
2020-04-01 11:53:23 +0900

    test

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,16 +2,27 @@ class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy]
   def index
     @blogs = Blog.all
-  end  
-  def new
-    @blog = Blog.new
   end
+
+  def new
+    # @blog = Blog.new
+    @blog = current_user.blogs.build
+  end
+
+  def confirm
+    # @blog = Blog.new(blog_params)
+    @blog = current_user.blogs.build(blog_params)
+    render :new if @blog.invalid?
+  end
+
   def show
-    @blog = Blog.find(params[:id])
   end
 
   def create
-    @blog = Blog.new(blog_params)
+    # @blog = Blog.new(blog_params)
+    @blog = current_user.blogs.build(blog_params)
+    
+    # @blog.user_id = current_user.id
     if params[:back]
       render :new
     else
@@ -22,9 +33,10 @@ class BlogsController < ApplicationController
       end
     end
   end
+
   def edit
-    @blog = Blog.find(params[:id])
   end
+
   def update
     @blog = Blog.find(params[:id])
     if @blog.update(blog_params)
@@ -33,14 +45,12 @@ class BlogsController < ApplicationController
       render :edit
     end
   end
+
   def destroy
     @blog.destroy
     redirect_to blogs_path, notice:"投稿を削除しました！"
   end
-  def confirm
-    @blog = Blog.new(blog_params)
-    render :new if @blog.invalid?
-  end
+  
   private
   def blog_params
     params.require(:blog).permit(:image, :content)
